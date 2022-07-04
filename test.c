997ef95ad6ac068a814d450fa16b14159f26f29f
W4terf1re <32442908+W4terf1re@users.noreply.github.com>
2022-07-04 10:09:36 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,17 +1,53 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-int main(){
+#include "unistd.h"
+int check1(){
     int input, x, i;
     char tmp1[8];
     char tmp2[8];
-    read(0, input, 4);
-    memset(x, 0, 4);
+    read(0, &input, 4);
+    x=0;
     for (i = 0; i < 32; i++){
         if (input & (1 << i)){
             x |= 1 << i;
         }
     }
     memcpy(tmp1,tmp2, x);
+    return 0;
+}
+int check2(void){
+    char input[4];
+    char x[4];
+    char c=0;
+    int check1, check2=0;
+    
+    read(0, &check1, 4);
+    read(0, input, 4);
+    memset(x, 0, 4);
+    while(c<32){
+        if(check1 & (1<<c)){
+            check2 |= (1<<c);
+        }
+        c++;
+    }
+    if(check2==0xdeadbeaf){
+        c=0;
+        while(c<32){
+            if((*(int*)input) & (1<<c)){
+                *(int*)x |= (1<<c);
+            }
+            c++;
+        }
+        // printf("%x\n", *(int*)x);
+        if(*(int*)x == 0xdeadbeaf){
+            memcpy(input, input + 10, 60);
+        }
+    }
+    return 0;
+}
+int main(void){
+    check1();
+    check2();
     return 0;
 }
